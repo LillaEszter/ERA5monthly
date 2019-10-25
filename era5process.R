022063ad773432c62de80ab99f67406ada724d6b
Péter Kalicz <kaliczp@gmail.com>
2019-10-25 12:12:36 +0200

    Módosítva az új lekérdezéshez
    Az új lekérdezés eredményeként létrejött fájlban megváltoztak
    a változó nevek. Az eredmény objektum nevét is átírtam.

--- era5process.R
+++ era5process.R
@@ -1,13 +1,13 @@
 library(ncdf4)
-hom <- nc_open("adaptor.mars.internal-1570982526.5532598-9802-27-ec441651-d2d9-43fb-8b59-22b1d12186d0.nc") 
+hom <- nc_open("TempMoreYearsSopron.nc") 
 aktdate <- as.Date("1900-01-01") + ncvar_get(hom,"time")/24
-which(ncvar_get(hom,"latitude") == 47.50) # 171
-which(ncvar_get(hom,"longitude") == 16.50) # 67
+which(ncvar_get(hom,"lat") == 47.50) # 2
+which(ncvar_get(hom,"lon") == 16.50) # 2
 
-homdata <- ncvar_get(hom,"t2m", start = c(67,171,1), count = c(1,1,12)) - 273.15
+homdata <- ncvar_get(hom,"t2m_NON_CDM", start = c(2, 2, 1), count = c(1, 1, 220)) - 273.15
 
 nc_close(hom)
 
 library(xts)
-sopron2002 <- xts(homdata, aktdate)
-plot(sopron2002)
+sopron <- xts(homdata, aktdate)
+plot(sopron)
