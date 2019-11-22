b45196d1ee67ea58604f861538a67b06ec394f2b
Lilla <slilla897@gmail.com>
2019-11-22 10:21:33 +0100

    csapadek javitott

--- Fuzer_csap.R
+++ Fuzer_csap.R
@@ -1,14 +1,15 @@
 library(ncdf4)
 csap <- nc_open("fuzer_csap.nc") 
-aktdate <- as.Date("2007-01-01") + ncvar_get(csap,"time")/24
-which(ncvar_get(csap,"lat") == 47.50) # 1
-which(ncvar_get(csap,"lon") == 21.45) # 2
+aktdate <- as.Date("1900-01-01") + ncvar_get(csap,"time")/24
+which(ncvar_get(csap,"lat") == 47.50) # 3
+which(ncvar_get(csap,"lon") == 21.4) # 5
+ncvar_get(csap,"lon")
 
-csapdata <- ncvar_get(csap,"t", start = c(2, 2, 1), count = c(1, 1, 220)) - 273.15
+csapdata <- ncvar_get(csap,"tp_NON_CDM", start = c(3, 5, 1), count = c(1, 1, 148))
 
 nc_close(csap)
 
 library(xts)
 fuzer <- xts(csapdata, aktdate)
-plot("Csapadék változása 2007-2019 Füzér és környéke",
+plot(fuzer, main="Csapadék változása 2007-2019 Füzér és környéke",
      xlab="idő", ylab="csapadék", type="o", col="orange")
